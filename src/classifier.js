32d748f3717798e31208aeb6d1b0833778ae2b68
chei7tain <fredrickokwuobi@gmail.com>
2021-10-27 15:28:09 +0100

    solved

--- src/classifier.js
+++ src/classifier.js
@@ -1,9 +1,68 @@
-/**
- * This is the entry point to the program
- * Question 1 - Classifier
- *
- * @param {any} input Array of student objects
- */
-function classifier(input) {}
 
+
+// /**
+//  * This is the entry point to the program
+//  * Question 1 - Classifier
+//  *
+//  * @param {any} input Array of student objects
+//  */
+
+function classifier(input) {
+  names = [...input];
+  names = names.map((member) => {
+    member.age = new Date().getFullYear() - new Date(member.dob).getFullYear();
+    return member;
+  });
+
+  names.sort((a, b) => a.age - b.age);
+
+  let groupedMembers = [];
+  // names;
+
+  let ageGroup = [];
+
+  let ageLimit = 0;
+
+  for (let person of names) {
+    if (ageLimit === 0) {
+      ageLimit = person.age;
+    }
+    if (person.age - ageLimit <= 5) {
+      if (ageGroup.length < 3) {
+        ageGroup.push(person);
+      } else {
+        groupedMembers.push(ageGroup);
+        ageGroup = [];
+        ageGroup.push(person);
+        ageLimit = person.age;
+      }
+    } else {
+      groupedMembers.push(ageGroup);
+      ageGroup = [];
+      ageGroup.push(person);
+      ageLimit = person.age;
+    }
+  }
+  console.log(ageGroup);
+  console.log(groupedMembers);
+
+  if (ageGroup.length) groupedMembers.push(ageGroup);
+  let result = {};
+
+  for (let i = 0; i < groupedMembers.length; i++) {
+    let str = 'group' + (i + 1);
+    let membersAges = groupedMembers[i].map((el) => el.age);
+    membersAges;
+    result[str] = {
+      members: groupedMembers[i],
+      oldest: Math.max(...membersAges),
+      sum: membersAges.reduce((prev, curr) => prev + curr),
+      regNos: groupedMembers[i]
+        .map((el) => parseInt(el.regNo))
+        .sort((a, b) => a - b),
+    };
+  }
+  result.noOfGroups = groupedMembers.length;
+  return result;
+}
 module.exports = classifier;
--- src/laundry.js
+++ src/laundry.js
@@ -4,6 +4,84 @@
  *
  * @returns {any} Trip data analysis
  */
-function getMaxPairs(noOfWashes, cleanPile, dirtyPile) {}
+
+function getMaxPairs(noOfWashes, cleanPile, dirtyPile) {
+  let cleanPileObj = {};
+  let dirtyPileObj = {};
+  let numberOfCleanPairs = 0;
+
+  for (let pile of cleanPile) {
+    if (cleanPileObj[pile]) {
+      cleanPileObj[pile]++;
+      if (cleanPileObj[pile] === 2) {
+        numberOfCleanPairs++;
+        cleanPileObj[pile] = 0;
+      }
+    } else {
+      cleanPileObj[pile] = 1;
+    }
+  }
+
+  for (let pile of cleanPile) {
+    if (cleanPileObj[pile] === 0) delete cleanPileObj[pile];
+  }
+  console.log(cleanPileObj);
+  for (let pile of dirtyPile) {
+    if (dirtyPileObj[pile]) {
+      dirtyPileObj[pile]++;
+    } else dirtyPileObj[pile] = 1;
+  }
+  console.log(numberOfCleanPairs);
+  let washed = false;
+  var timesWashed = 0;
+  for (let timesToWash = 0; timesToWash < noOfWashes; timesToWash++) {
+    if (Object.keys(cleanPileObj).length > 0) {
+      for (let cleanSocks in cleanPileObj) {
+        if (dirtyPileObj[cleanSocks]) {
+          dirtyPileObj[cleanSocks]--;
+          numberOfCleanPairs++;
+          if (dirtyPileObj[cleanSocks] === 0) delete dirtyPileObj[cleanSocks];
+          delete cleanPileObj[cleanSocks];
+          washed = true;
+          break;
+        } else {
+          washed = false;
+          delete cleanPileObj[cleanSocks];
+        }
+      }
+    } else {
+      washed = false;
+      timesWashed = timesToWash;
+      break;
+    }
+    if (Object.keys(cleanPileObj).length <= 0) {
+      timesWashed = washed ? timesToWash + 1 : timesToWash;
+      break;
+    }
+    console.log('RRR:' + timesToWash);
+    console.log('RRRTT:' + numberOfCleanPairs);
+  }
+  console.log('RRRSSS:' + timesWashed + ' numClean: ' + numberOfCleanPairs);
+  for (let dirtySocks in dirtyPileObj) {
+    while (dirtyPileObj[dirtySocks] >= 2 && noOfWashes - timesWashed >= 2) {
+      dirtyPileObj[dirtySocks] -= 2;
+      console.log('DDD:' + dirtyPileObj[dirtySocks]);
+      console.log('TTT:' + timesWashed);
+      numberOfCleanPairs++;
+      timesWashed += 2;
+      console.log('NNN:' + numberOfCleanPairs);
+    }
+  }
+
+  return numberOfCleanPairs;
+}
+
+const numberMachineCanWash = 0;
+
+const cleanPile = [10, 11, 12, 11, 10, 10, 13, 11, 12, 10, 13, 14, 11, 10, 12];
+
+const dirtyPile = [10, 10, 11, 12, 13, 10, 14, 14, 14, 12, 12, 10, 10, 11, 11];
+
+console.log(getMaxPairs(numberMachineCanWash, cleanPile, dirtyPile));
 
 module.exports = getMaxPairs;
